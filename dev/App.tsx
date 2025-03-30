a9a34faa8f86b290cf0645f7464d94d339591568
bigmistqke <vandijckv@gmail.com>
2025-03-30 17:22:29 +0200

    update demo

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,38 +1,29 @@
 import { Repeat } from '@solid-primitives/range'
-import { createSignal, type Component } from 'solid-js'
+import { type Component } from 'solid-js'
+import { createStore } from 'solid-js/store'
 import { HexEditor } from 'src'
 import styles from './App.module.css'
 
 const App: Component = () => {
-  const [array, setArray] = createSignal(
-    new Uint8Array(Array.from({ length: 50 * 50 * 3 }, (_, i) => i % 255)),
-    { equals: false },
+  const [array, setArray] = createStore(
+    new Uint8Array(Array.from({ length: 50 * 50 * 3 }, (_, i) => Math.floor(Math.random() * 255))),
   )
   return (
     <div class={styles.app}>
       <div class={styles.canvas}>
-        <Repeat times={array().length / 3}>
+        <Repeat times={array.length / 3}>
           {index => (
             <span
               style={{
-                background: `rgb(${array()[index * 3]}, ${array()[index * 3 + 1]}, ${
-                  array()[index * 3 + 2]
+                background: `rgb(${array[index * 3]}, ${array[index * 3 + 1]}, ${
+                  array[index * 3 + 2]
                 })`,
               }}
             />
           )}
         </Repeat>
       </div>
-      <HexEditor
-        class={styles.editor}
-        array={array()}
-        onArrayUpdate={(index, value) => {
-          setArray(array => {
-            array[index] = value
-            return array
-          })
-        }}
-      />
+      <HexEditor class={styles.editor} array={array} onArrayUpdate={setArray} />
     </div>
   )
 }
